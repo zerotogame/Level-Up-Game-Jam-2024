bb6f143d575bd4cc657bc212b4e3977addf052bf
VictorReverte-DAW <mevictor@hotmail.es>
2024-07-02 22:00:31 +0200

    M1 - Vista de tablero

---
+++ Assets/Scripts/BuildingPoint.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingPoint : MonoBehaviour
+{
+    public Building currentBuilding; // El prefab que quieres instanciar
+    public Card cardPrefab; // El prefab de la carta
+}
