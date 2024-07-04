7848e1f7aa1fb82dee693f07d21cc5de43be7097
GustavoMendozaAren <stavmdza@gmail.com>
2024-07-03 20:44:43 -0600

    MenuDePausaImplementado
    Funciona con botón

--- Assets/Scripts/BarraScripts/BarraLocura.cs
+++ Assets/Scripts/BarraScripts/BarraLocura.cs
@@ -2,12 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class BarraLocura : MonoBehaviour
 {
 
     public Image barraLocura;
+    public GameObject pausePanel, victoryPanel, defeatPanel;
 
+    private void Start()
+    {
+        GameContStat.velocidadPersonajesLoki = 2f;
+        GameContStat.velocidadPersonajesVeles = 2f;
+        GameContStat.velocidadPersonajesEris = 2f;
+        GameContStat.velocidadPersonajesCthulhu = 2f;
+}
+
+    // -------PANELES BOTONES--------------
+    public void PauseButton() 
+    { 
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void PauseButtonBack()
+    {
+        pausePanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void MenuButton()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+        Time.timeScale = 1f;
+    }
+    public void SalirButton()
+    {
+        Application.Quit();
+    }
+
+
+
+    // -------PERONAJES BOTONES--------------
     public void LokiMasLocura() 
     {
         barraLocura.fillAmount += .05f;
