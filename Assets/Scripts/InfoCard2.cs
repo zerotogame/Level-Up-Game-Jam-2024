2176525d40a2651266755e9e55090f4902b906c6
GustavoMendozaAren <stavmdza@gmail.com>
2024-07-05 18:59:23 -0600

    CartasDorsalesConPanelesYAnimacion

--- Assets/Scripts/InfoCard2.cs
+++ Assets/Scripts/InfoCard2.cs
@@ -4,11 +4,17 @@ using UnityEngine;
 
 public class InfoCard2 : MonoBehaviour
 {
-    private GameObject velesDorsalPanel;
+    private GameObject[] panelesDioses = new GameObject[11];
+
     bool pressed = false;
+    public float CardIDInfo;
     private void Start()
     {
-        velesDorsalPanel = GameObject.Find("VelesDorsalPanel");
+        panelesDioses[0] = GameObject.Find("LokiDorsalPanel");
+        panelesDioses[3] = GameObject.Find("ErisDorsalPanel");
+        panelesDioses[6] = GameObject.Find("VelesDorsalPanel");
+        panelesDioses[9] = GameObject.Find("CthulhuDorsalPanel");
+
     }
 
     private void OnMouseDown()
@@ -23,15 +29,58 @@ public class InfoCard2 : MonoBehaviour
 
     private void OnMouseOver()
     {
-        if (!pressed) 
-            velesDorsalPanel.gameObject.transform.GetChild(0).gameObject.SetActive(true);
-        else
-            velesDorsalPanel.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        LokiDorsalPanelMethod();
+        ErisDorsalPanelMethod();
+        VelesDorsalPanelMethod();
+        CthulhuDorsalPanelMethod();
     }
 
     private void OnMouseExit()
     {
-        velesDorsalPanel.gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        panelesDioses[0].gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        panelesDioses[3].gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        panelesDioses[6].gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        panelesDioses[9].gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+    private void LokiDorsalPanelMethod()
+    {
+        if (CardIDInfo == 0)
+        {
+            if (!pressed)
+                panelesDioses[0].gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            else
+                panelesDioses[0].gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+    private void ErisDorsalPanelMethod()
+    {
+        if (CardIDInfo == 3)
+        {
+            if (!pressed)
+                panelesDioses[3].gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            else
+                panelesDioses[3].gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+    private void VelesDorsalPanelMethod() 
+    {
+        if (CardIDInfo==6) 
+        {
+            if (!pressed)
+                panelesDioses[6].gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            else
+                panelesDioses[6].gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+    private void CthulhuDorsalPanelMethod()
+    {
+        if (CardIDInfo == 9)
+        {
+            if (!pressed)
+                panelesDioses[6].gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            else
+                panelesDioses[6].gameObject.transform.GetChild(0).gameObject.SetActive(false);
+        }
     }
 
 }
