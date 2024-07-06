38679bab0ad5d62e80616fe6fd915c3b289ce074
GustavoMendozaAren <stavmdza@gmail.com>
2024-07-06 08:26:50 -0600

    TutorialEnGamePlay

--- Assets/Scripts/GameController/GameplayController.cs
+++ Assets/Scripts/GameController/GameplayController.cs
@@ -4,8 +4,38 @@ using UnityEngine;
 
 public class GameplayController : MonoBehaviour
 {
+    // Tutorial Paneles
+    public GameObject panelTut1, panelTut2, panelTut3, panelTut4;
+
     public GameObject panelOpciones, panelPausa;
     public GameObject textoEstadoActivoOver, textoEstadoInactivoOver;
+
+    // -------TUTORIAL BOTONES Y PANELES------------
+    private void Start()
+    {
+        Time.timeScale = 0f;
+    }
+    public void Continuar1Bttn() 
+    {
+        panelTut1.SetActive(false);
+        panelTut2.SetActive(true);
+    }
+    public void Continuar2Bttn()
+    {
+        panelTut2.SetActive(false);
+        panelTut3.SetActive(true);
+    }
+    public void Continuar3Bttn()
+    {
+        panelTut3.SetActive(false);
+        panelTut4.SetActive(true);
+    }
+    public void Continuar4Bttn()
+    {
+        panelTut4.SetActive(false);
+        Time.timeScale = 1.0f;
+    }
+
 
     // -------PANELES BOTONES--------------
     public void PauseButton()
